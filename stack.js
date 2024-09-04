439d8aa198ffb2cdd391b38d21a991789e534402
dev-md-rajib <rajibmiah978@gmail.com>
2024-09-04 17:38:20 +0600

    Stack details are added

---
+++ stack.js
@@ -0,0 +1,16 @@
+/**
+ * Js doesn't have built in stack
+ * Instead the array contains methods that can used to operate LIFO operations
+ * Array has push() and pop() methods/functions, these can be used to the array
+ * as a stack.
+ */
+
+let stack = [];
+//push
+stack.push(1);
+
+//accessing top element
+stack[stack.length - 1];
+
+//pop
+stack.pop();
